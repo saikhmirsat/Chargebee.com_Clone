825cc452e623d71c3f6e024a2a9ac6eb70e755c5
saikhmirsat <saikh.mirsat.786@gmail.com>
2022-11-13 18:34:00 +0530

    complete

--- synonymous-wall/src/AllRoutes.jsx
+++ synonymous-wall/src/AllRoutes.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Customers from './Pages/Customers'
+import DemoPage from './Pages/DemoPage'
 import Home from './Pages/Home'
 import Login from './Pages/Login'
 import Partner from './Pages/Partner'
 import Pricing from './Pages/Pricing'
 import Signup from './Pages/Signup'
+import CustomerDetalPage from './SinglePage/CustomerDetalPage'
+import SyncSpiderDetail from './SinglePage/SyncSpiderDetail'
+
+
 
 
 
@@ -18,6 +23,9 @@ export default function AllRoutes() {
             <Route path='/partner' element={<Partner />}></Route>
             <Route path='/login' element={<Login />}></Route>
             <Route path='/signup' element={<Signup />}></Route>
+            <Route path='/customerDetailPage' element={<CustomerDetalPage />}></Route>
+            <Route path='/customers/SyncSpider' element={<SyncSpiderDetail />}></Route>
+            <Route path='/demoPage' element={<DemoPage />}></Route>
         </Routes>
     )
 }
---
+++ synonymous-wall/src/Pages/DemoPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './DemoPage.css'
+
+export default function DemoPage() {
+    return (
+        <div className='demoPage-main-container'>
+            {/* <img style={{ width: '100%' }} src="https://i.ibb.co/S60tF5Y/Screenshot-2022-11-13-at-5-48-21-PM.png" alt="Screenshot-2022-11-13-at-5-48-21-PM" /> */}
+        </div>
+    )
+}
